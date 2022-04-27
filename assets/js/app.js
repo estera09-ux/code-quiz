e8a4257fdffdc9243b666c7bbdb1a652f0983599
Estera Bechian <esterabechian12@hotmail.com>
2022-04-27 20:38:38 +0100

    targeted the id's

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,8 @@
 console.log("hey");
+
+const startQuiz = document.getElementById("start");
+const startButton = document.getElementById("start-btn");
+const questionLine = document.querySelector(".banner");
+const sectionQuestion = document.getElementById("question-section");
+const listAnswer = document.getElementById("list");
+const timerSection = document.getElementById("timer");
